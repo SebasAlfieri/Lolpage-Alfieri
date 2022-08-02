8be14f94557353a7487190a9a5803c7bc8eac592
Sebastian Alfieri <sebasalfieri@gmail.com>
2022-08-02 13:57:14 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-alert('Esta página fue entrega final al curso de Coderhouse\nNo presenta un diseño ideal ya que la idea era implementar casi todos los conocimientos por mas que sea de manera forzadada para cumplir las expectativas del trabajo.')
+alert('Esta página fue entrega final al curso de Coderhouse\nNo presenta un diseño ideal ya que la idea era implementar casi todos los conocimientos por mas que sea de manera forzadada para cumplir las expectativas del trabajo.');
